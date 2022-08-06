0c74596aef6cd84140d951f9ddbc17c47c5de858
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-08-06 14:13:41 +0300

    feat(solid-button): implement solid button

--- src/views/content/support-open-source/support-open-source.ts
+++ src/views/content/support-open-source/support-open-source.ts
@@ -4,8 +4,6 @@ import { paragraphTitles } from '../../../shared/constants/titles';
 import { ParagLink } from '../../ui/parag-link/parag-link';
 import SOSTexts from '../../../shared/constants/support-open-source/texts'; 
 
-import { SolidButton } from '../../ui/buttons/solid-button/solid-button';
-
 import './support-open-source.css';
 
 export const SupportOpenSource = {
@@ -32,10 +30,5 @@ export const SupportOpenSource = {
         ),
         m('h4.sos-text__h4', SOSTexts.donations.thanks),
       ]),
-      m(SolidButton, {
-        label: 'Recalculate',
-        bgColor: 'button-blue',
-        labelColor: 'white',
-      }),
     ]),
 }
