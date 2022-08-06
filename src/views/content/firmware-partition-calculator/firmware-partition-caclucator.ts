3e1810a22d9a577a677ca480b437e3c089003d74
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-08-06 14:17:06 +0300

    feat(partit-calc):WIP implement firmware-partition-calc

--- src/views/content/firmware-partition-calculator/firmware-partition-caclucator.ts
+++ src/views/content/firmware-partition-calculator/firmware-partition-caclucator.ts
@@ -1,24 +1,12 @@
 import m from 'mithril';
+import { CalcTable } from '../../ui/calc-table/calc-table';
 
 import './firmware-partition-calculator.css';
 
 export const FirmwarePartitionCalculator = {
   view: () =>
     m('section.fw-prtn-calc', [
-      m('.calc-table', 
-        m('.calc-table__cell.cell.cell_1-1',
-          m('label.cell__label', 'MTD device name'),
-          m('input.cell__input.cell__input_name'),
-        ),
-        m('.calc-table__cell.cell_1-2'),
-        m('.calc-table__cell.cell_1-3'),
-        m('.calc-table__cell.cell_1-4'),
-        m('.calc-table__cell.cell_2-1'),
-        m('.calc-table__cell.cell_2-2'),
-        m('.calc-table__cell.cell_2-3'),
-        m('.calc-table__cell.cell_2-4'),
-        m('.calc-table__cell.cell_2-5'),
-      ),
+      m(CalcTable),
       m('.calc-progress-bar', ),
       m('.calc-msg', ),
     ]),
